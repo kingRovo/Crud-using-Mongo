3b1b199d61737fac65b21567db859bc984090728
updesh <updesh.yadav@bigohtech.com>
2022-02-16 16:25:23 +0530

    update Https error Msgs

--- src/main/java/com/rovo/mongocrud/controller/UserController.java
+++ src/main/java/com/rovo/mongocrud/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
             return  new ResponseEntity<List<User>>(users,HttpStatus.OK);
         }
         catch (Exception e){
-            return  new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+            return  new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
         }
     }
 
